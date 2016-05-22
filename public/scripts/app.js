59dd469112493eb50f43422df2a46d2214bf5134
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-05-22 11:14:02 -0700

    moved scripts and styles folders into public directory

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-  console.log("Sanity Check! app.js running!")
+  console.log("Sanity Check! app.js running!");
 
   //use ajax and jquery to render projects here
   //maybe you will need a model, route and controller for that?
