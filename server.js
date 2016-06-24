f9aeef6eb2022aee58f931eb5c2fb86ca8efef24
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2016-06-23 22:05:36 -0400

    example parse

--- server.js
+++ server.js
@@ -5,7 +5,15 @@ var mongoose = require('mongoose');
 
 // calling express in app variable.
 var app = express();
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || 'myMasterKey'
+});
 
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
 
 /**********
 SERVER
