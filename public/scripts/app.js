119a1686cc8c0061cb49e59d7041f7235073d468
Jacquelyn Fambrough <jacquelynfambrough@gmail.com>
2017-02-21 15:49:35 -0500

    title text now properly centers in mobile viewports:

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,8 @@
 $(document).ready(function(){
   console.log("Sanity Check! app.js running!");
-  // 
+  //
   // $(window).on('scroll', function(event){
   //   $('h1').playAnimation('exit');
   // });
+
 });
